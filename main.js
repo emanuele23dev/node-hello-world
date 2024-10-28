5f114ace375b42c6f0feb456ff48f5ac867a5c1e
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-28 19:11:51 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Iniziate creando un file main.js con un semplice console.log che dica hello node js
+
+// fate girare lo script usando node main.js
+
+// aggiungete allo script una variabile di ambiente chiamata PIN  con associato numero a piacere es. PIN=666
+
+// usate il modulo process per recuperare e stampare in console dalla proprietá env la variabile PIN
+
+// ora passate un argomento al vostro script e recuperatene il valore usando process e la proprietá argv per stamparlo in console
+
+console.log('hello node js');
